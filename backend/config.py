9f6f48760b235b25a4a04998336d0004464a14c2
spark <ses8621@gmail.com>
2019-11-21 10:51:56 -0800

    install db and auth related packages and update config.Config

--- backend/config.py
+++ backend/config.py
@@ -7,3 +7,9 @@ class Config():
     # general config
     SECRET_KEY = os.environ.get('SECRET_KEY')
     
+    # authentication
+    JWT_AUTH_URL_RULE = '/api/auth'
+
+    # database
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
