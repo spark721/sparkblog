dee84c85521f26f323e18e06fa5b3bc913adb385
spark <ses8621@gmail.com>
2019-11-26 21:38:11 -0800

    define Post model and post_fields

---
+++ backend/api/models/post.py
@@ -0,0 +1,34 @@
+from flask_restful import fields
+from api.extensions import db, bcrypt
+from datetime import datetime
+
+
+post_fields = {
+    'id': fields.Integer,
+    'title': fields.String,
+    'body': fields.String,
+}
+
+
+class Post(db.Model):
+    """Model for posts."""
+    __tablename__ = 'posts'
+
+    # define columns
+    id = db.Column(db.Integer,
+                    primary_key=True)
+    title = db.Column(db.String(),
+                        nullable=False)
+    body = db.Column(db.String(),
+                        nullable=False)
+    created = db.Column(db.DateTime,
+                        default=datetime.utcnow,
+                        nullable=False)
+
+    def __init__(self, title, body, **kwargs):
+        super(Post, self).__init__(**kwargs)
+        self.title = title
+        self.body = body
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
