de515a357c89a5899e8444556fdff529a25569ad
spark <ses8621@gmail.com>
2019-11-26 22:01:32 -0800

    add foreignkey to Post table

--- backend/api/models/post.py
+++ backend/api/models/post.py
@@ -18,8 +18,12 @@ class Post(db.Model):
     id = db.Column(db.Integer,
                     primary_key=True)
     title = db.Column(db.String(),
+                        unique=True,
                         nullable=False)
     body = db.Column(db.String(),
+                        nullable=False)
+    author_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id'),
                         nullable=False)
     created = db.Column(db.DateTime,
                         default=datetime.utcnow,
